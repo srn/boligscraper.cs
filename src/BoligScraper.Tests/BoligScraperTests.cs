92e6d99b046c1c09d6b8bbd2b95aadc47d504e35
Soren Rasmussen <brokzftw@gmail.com>
2012-09-03 19:29:22 +0200

    Added mapped properties

---
+++ src/BoligScraper.Tests/BoligScraperTests.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+using RestSharp;
+
+namespace BoligScraper.Tests
+{
+    [TestFixture]
+    public class BoligScraperTests
+    {
+        private Scraper _boligScraper;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _boligScraper = new Scraper();
+        }
+
+        [Test]
+        public void CanCreateBoligPortalRequestAndDeserializeData()
+        {
+            // Arrange
+            var boligPortalRequest = new BoligPortalRequest
+                                         {
+                                             Amt = AmtEnum.Aarhus,
+                                             RentMin = "0",
+                                             RentMax = "10000",
+                                             ZipCodes = new List<int> {8000, 8200},
+                                             ApartmentType = new List<string> {"1", "2", "3", "4"},
+                                             RentLength = new List<string> {"4"},
+                                             Page = "1",
+                                             Limit = "15",
+                                             SortCol = "3",
+                                             SortDesc = "1"
+                                         };
+
+            // Act
+            IRestResponse restResponse = null;
+            BoligPortalResponse boligPortalResponse = _boligScraper.Scrape(boligPortalRequest, out restResponse);
+
+            // Assert
+            Assert.That(restResponse, Is.Not.Null);
+            Assert.That(boligPortalRequest, Is.Not.Null);
+            Assert.That(boligPortalResponse.Properties.Count, Is.Not.EqualTo(0));
+        }
+    }
+}

6353b07ba4883e566ee58f9bf11dc6a53b0c88da
Søren Rasmussen <brokzftw@gmail.com>
2012-09-04 21:14:08 +0200

    Added unit test for comparing two lists and only returning new ones

--- src/BoligScraper.Tests/BoligScraperTests.cs
+++ src/BoligScraper.Tests/BoligScraperTests.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using NUnit.Framework;
 using RestSharp;
 
@@ -41,6 +42,26 @@ namespace BoligScraper.Tests
             Assert.That(restResponse, Is.Not.Null);
             Assert.That(boligPortalRequest, Is.Not.Null);
             Assert.That(boligPortalResponse.Properties.Count, Is.Not.EqualTo(0));
+        }
+
+        [Test]
+        public void CanCompareOldIdsWithNewIdsAndReturnNewIdsOnly()
+        {
+            // Arrange
+            var oldIds = new List<int> { 123, 1234, 123456, 1233 };
+            var newIds = new List<int> { 123, 1234, 12345 };
+
+            // Act
+            var mergedIds = new List<int>();
+            mergedIds.AddRange(oldIds);
+            mergedIds.AddRange(newIds);
+
+            IList<int> except = mergedIds.Except(oldIds).ToList();
+
+            // Assert
+            Assert.That(except, Is.Not.Null);
+            Assert.That(except.Count(), Is.EqualTo(1));
+            Assert.That(except.SingleOrDefault(), Is.EqualTo(12345));
         }
     }
 }
